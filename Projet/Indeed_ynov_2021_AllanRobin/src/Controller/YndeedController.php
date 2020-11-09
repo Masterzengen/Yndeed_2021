79c6501e08b107ecb2d24d85f0e53775f9902b17
Masterzengen <72359208+Masterzengen@users.noreply.github.com>
2020-11-09 23:16:04 +0100

    Site avec base et postuler

--- Projet/Indeed_ynov_2021_AllanRobin/src/Controller/YndeedController.php
+++ Projet/Indeed_ynov_2021_AllanRobin/src/Controller/YndeedController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Offres;
 
 class YndeedController extends AbstractController
 {
@@ -13,8 +14,12 @@ class YndeedController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Offres::class);
+        $offres = $repo->findAll();
+
         return $this->render('yndeed/offres.html.twig', [
             'controller_name' => 'YndeedController',
+            'offres' => $offres
         ]);
     }
 
@@ -28,5 +33,30 @@ class YndeedController extends AbstractController
             'yndeed/home.html.twig',
             ['title' => "Salutations!"]
         );
+    }
+
+    /**
+     * @Route ("/offres/{id}", name="offreShow")
+     */
+
+    public function show($id)
+    {
+        $repo = $this->getDoctrine()->getRepository(Offres::class);
+        $offre = $repo->find($id);
+        return $this->render('yndeed/show.html.twig', [
+            'offre' => $offre
+        ]);
+    }
+
+     /**
+     * @Route ("/offres/postuler/{id}", name="postuler")
+     */
+
+    public function postuler($id){
+        $repo = $this->getDoctrine()->getRepository(Offres::class);
+        $offre = $repo->find($id);
+        return $this->render('yndeed/postuler.html.twig',[
+            'offre'=> $offre
+        ]);
     }
 }
