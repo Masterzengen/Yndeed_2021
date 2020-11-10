d8d3eaf6ddfaaaacd52204dd60e956f83951594d
Masterzengen <72359208+Masterzengen@users.noreply.github.com>
2020-11-10 10:34:37 +0100

    Add Faker

--- Projet/Indeed_ynov_2021_AllanRobin/src/DataFixtures/AppFixtures.php
+++ Projet/Indeed_ynov_2021_AllanRobin/src/DataFixtures/AppFixtures.php
@@ -5,23 +5,27 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Offres;
+use Faker\Factory;
+use Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Faker\Factory::create('fr_FR');
+
         for ($i = 1; $i <= 10; $i++) {
             $offre = new Offres();
-            $offre->setTitle("$i ème Offre incoyable")
-                ->setDescription("Description du pif provisoire")
-                ->setCodePostal("$i$i - $i$i$i")
-                ->setAdresse("$i rue de la ménagère boiteuse")
-                ->setVille("$i-City")
+            $offre->setTitle("Offre incoyable n°$i")
+                ->setDescription($faker->text)
+                ->setCodePostal($faker->postcode)
+                ->setAdresse($faker->streetAddress)
+                ->setVille($faker->city)
                 ->setDateDeCreation(new \DateTime())
                 ->setDateDeMiseAJour(new \DateTime())
                 ->setFinDeLaMission(new \DateTime())
                 ->setTypeDeContrat("Work in progress")
-                ->setContrat("Work in progress");
+                ->setContrat($faker->randomElement($array = array ('a','b','c')));
             $manager->persist($offre);
         }
         // $product = new Product();
