94977f2e05ae7ad4cc030ff179731b4fa0e20c54
Masterzengen <72359208+Masterzengen@users.noreply.github.com>
2020-11-23 11:03:40 +0100

    form defect

--- Projet/Indeed_ynov_2021_AllanRobin/src/Form/OffreType.php
+++ Projet/Indeed_ynov_2021_AllanRobin/src/Form/OffreType.php
@@ -2,10 +2,13 @@
 
 namespace App\Form;
 
+
 use App\Entity\Offres;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 
 class OffreType extends AbstractType
 {
@@ -21,6 +24,24 @@ class OffreType extends AbstractType
             ->add('typeContrat')
             ->add('contrat')
         ;
+        $builder->get('contrat')->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event){
+            $data = $event->getData();
+            dump($data);
+            });
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            
+            //dd($data->getContrat());
+            // if($data->getContrat()->getName() != null){
+            //       dd($data); 
+            // }
+            
+         
+            //if(strcmp($data->contrat, 'CDI') != 0){
+             
+
+            //}
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
