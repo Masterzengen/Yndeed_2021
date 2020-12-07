9431bcdc0b2cc36202feb41f3eccee30703d9c46
Masterzengen <72359208+Masterzengen@users.noreply.github.com>
2020-12-07 11:09:57 +0100

    S'inscrire+pushhash

--- Projet/Indeed_ynov_2021_AllanRobin/src/Controller/LoginController.php
+++ Projet/Indeed_ynov_2021_AllanRobin/src/Controller/LoginController.php
@@ -9,18 +9,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class LoginController extends AbstractController
 {
     /**
      * @Route("/inscription", name="Inscription")
      */
-    public function register(Request $request, EntityManagerInterface $em)
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
+            $hash = $encoder->encodePassword($user,$user->getPassword());
+            $user->setPassword($hash);
             $em->persist($user);
             $em->flush();
         }
