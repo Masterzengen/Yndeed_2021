114e1c138583edc49b2c3c81dfe8978f2e26881d
Masterzengen <72359208+Masterzengen@users.noreply.github.com>
2020-12-07 11:01:37 +0100

    S'inscrire+push

--- Projet/Indeed_ynov_2021_AllanRobin/src/Controller/LoginController.php
+++ Projet/Indeed_ynov_2021_AllanRobin/src/Controller/LoginController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +15,15 @@ class LoginController extends AbstractController
     /**
      * @Route("/inscription", name="Inscription")
      */
-    public function register(): Response
+    public function register(Request $request, EntityManagerInterface $em)
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($user);
+            $em->flush();
+        }
         return $this->render('login/registration.html.twig', [
             'form' => $form->createView(),
         ]);
--- Projet/Indeed_ynov_2021_AllanRobin/src/Form/RegistrationType.php
+++ Projet/Indeed_ynov_2021_AllanRobin/src/Form/RegistrationType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,7 @@ class RegistrationType extends AbstractType
             ->add('firstName')
             ->add('lastName')
             ->add('email')
-            ->add('password')
+            ->add('password',PasswordType::class)
         ;
     }
 
